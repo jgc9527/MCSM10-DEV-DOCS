09a7fbd4dbe34c2bd82a12adf3f1fd8eccd019c2
unitwk <unitwk@outlook.com>
2023-08-07 11:48:22 +0800

    Feat: 框架国际化

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,20 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
 import AppHeader from "./components/AppHeader.vue";
+import zhCN from "ant-design-vue/es/locale/zh_CN";
+import dayjs from "dayjs";
+import "dayjs/locale/zh-cn";
+import { ref } from "vue";
+dayjs.locale("zh-cn");
+
+const locale = ref(zhCN);
 </script>
 
 <template>
-  <div class="global-app-container">
-    <AppHeader></AppHeader>
-    <RouterView :key="$route.fullPath" />
-  </div>
+  <a-config-provider :locale="locale">
+    <div class="global-app-container">
+      <AppHeader></AppHeader>
+      <RouterView :key="$route.fullPath" />
+    </div>
+  </a-config-provider>
 </template>
