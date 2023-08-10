b996db5617707f6c2b29676cb69b6634fcf69120
unitwk <unitwk@outlook.com>
2023-08-10 20:09:04 +0800

    Feat: 数据总览

--- src/widgets/PanelOverview.vue
+++ src/widgets/PanelOverview.vue
@@ -1,10 +1,57 @@
 <script setup lang="ts">
+import DataStatistic from "@/components/DataStatistic.vue";
 import { t } from "@/lang/i18n";
 import type { LayoutCard } from "@/types";
 
 const props = defineProps<{
   card: LayoutCard;
 }>();
+
+const overviewList = [
+  {
+    title: t("计算机型号"),
+    value: "6666",
+  },
+  {
+    title: t("计算机型号"),
+    value: "6666",
+  },
+  {
+    title: t("计算机型号"),
+    value: "6666",
+  },
+  {
+    title: t("计算机型号"),
+    value:
+      "6666oasihdiah23oi1u28921938y1298312983h2189h 219h3 9812h9 18232 h8192 13h9812 39812",
+  },
+  {
+    title: t("计算机型号"),
+    value:
+      "6666oasihdiah23oi1u28921938y1298312983h2189h 219h3 9812h9 18232 h8192 13h9812 39812",
+  },
+  {
+    title: t("计算机型号"),
+    value:
+      "6666oasihdiah23oi1u28921938y1298312983h2189h 219h3 9812h9 18232 h8192 13h9812 39812",
+  },
+  {
+    title: t("计算机型号"),
+    value: "6666",
+  },
+  {
+    title: t("计算机型号"),
+    value: "6666",
+  },
+  {
+    title: t("计算机型号"),
+    value: "6666",
+  },
+  {
+    title: t("计算机型号"),
+    value: "6666",
+  },
+];
 </script>
 
 <template>
@@ -12,9 +59,16 @@ const props = defineProps<{
     <CardPanel style="height: 100%">
       <template #title>{{ card.title }}</template>
       <template #body>
-        <a-row>
-          <a-col :span="24" :md="12" :lg="6">
-            <div></div>
+        <a-row :gutter="[24, 24]">
+          <a-col
+            :span="12"
+            :md="12"
+            :lg="6"
+            v-for="item in overviewList"
+            :key="item.value"
+          >
+            <DataStatistic :title="item.title" :value="item.value">
+            </DataStatistic>
           </a-col>
         </a-row>
       </template>
