ccaf22b6d312b2b7b767603fe17cef20ba8266cf
alongw <alongw@alongw.cn>
2023-08-18 20:35:43 +0800

    Refactor: 重构 QuickStark 卡片

--- src/widgets/QuickStart.vue
+++ src/widgets/QuickStart.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import ActionButton from "@/components/ActionButton.vue";
 import { t } from "@/lang/i18n";
 import type { LayoutCard } from "@/types";
 import { router } from "@/config/router";
@@ -56,39 +57,16 @@ const actions = [
 </script>
 
 <template>
-  <CardPanel style="height: 100%">
+  <card-panel style="height: 100%">
     <template #title>{{ card.title }}</template>
     <template #body>
       <a-row :gutter="[24, 24]">
-        <a-col :span="24" v-for="action in actions" :key="action.title">
-          <div class="quick-btn" @click="action.click">
-            <div>
-              <component :is="action.icon"></component>
-            </div>
-            <div class="ml-8">{{ action.title }}</div>
-          </div>
-        </a-col>
+        <action-button
+          v-for="(action, key) in actions"
+          :key="action.title"
+          :action="actions[key]"
+        />
       </a-row>
     </template>
-  </CardPanel>
+  </card-panel>
 </template>
-
-<style lang="scss" scoped>
-.quick-btn {
-  width: 100%;
-  padding: 20px 12px;
-  border: 1px solid var(--color-gray-2);
-  background-color: var(--color-gray-2);
-
-  cursor: pointer;
-  transition: all 0.4s;
-  border-radius: 4px;
-
-  display: flex;
-}
-
-.quick-btn:hover {
-  border: 1px solid var(--color-gray-8);
-  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.16);
-}
-</style>
