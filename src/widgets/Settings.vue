ebe2cf4a17a4ea44b896e419d7d9c7bf66d429c9
unitwk <unitwk@outlook.com>
2023-08-19 11:51:50 +0800

    Feat: API 缓存

--- src/widgets/Settings.vue
+++ src/widgets/Settings.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import type { LayoutCard } from "@/types";
 import { ref } from "vue";
+import { userService } from "@/services/user";
 
 const props = defineProps<{
   card: LayoutCard;
@@ -10,18 +11,21 @@ const activeKey = ref("1");
 const formData = ref({
   language: "en_US",
 });
+
+const { getUserInfo } = userService();
+const { state, isLoading } = getUserInfo("666");
+getUserInfo("777");
 </script>
 
 <template>
   <CardPanel class="CardWrapper" style="height: 100%">
     <template #title>{{ card.title }}</template>
     <template #body>
+      <div>{{ state }}, {{ isLoading }}</div>
+
       <a-form :model="formData" layout="vertical">
         <a-form-item label="面板语言">
-          <a-input
-            v-model:value="formData.language"
-            placeholder="input placeholder"
-          />
+          <a-input v-model:value="formData.language" placeholder="input placeholder" />
         </a-form-item>
       </a-form>
     </template>
